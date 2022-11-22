aedd20d0122c91f95fab72d91254d5167a9b9ccb
Rodolfo Campos <rodolfo123@gmail.com>
2022-11-22 14:36:54 -0300

    added_user_id_to_products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_065324) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_172240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_065324) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "sales", force: :cascade do |t|
@@ -40,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_065324) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "users"
   add_foreign_key "sales", "products"
   add_foreign_key "sales", "users"
 end
