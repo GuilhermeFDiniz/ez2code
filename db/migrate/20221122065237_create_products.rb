b4601ab2141ba212a666b46f015f8ef9918624a6
Rodolfo Campos <rodolfo123@gmail.com>
2022-11-22 16:44:29 -0300

    droped tables and migratred again with devise users

--- db/migrate/20221122065237_create_products.rb
+++ db/migrate/20221122065237_create_products.rb
@@ -2,8 +2,11 @@ class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.text :info
+      t.text :details
       t.float :price
+      t.string :category
+      t.references :user, null: false, foreign_key: true
+
 
       t.timestamps
     end
